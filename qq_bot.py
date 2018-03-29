dafc17dbb51105423b9c14d3b1316bd813a1bea8
taoting <335354289@qq.com>
2018-03-29 13:44:56 +0800

    fix bugs

--- qq_bot.py
+++ qq_bot.py
@@ -5,57 +5,59 @@ from function import *
 
 @qqbotslot
 def onQQMessage(bot, contact, member, content):
-    if not bot.isMe(contact, member) and contact.ctype == "buddy":
-        if "帮助" in content:
-            bot.SendTo(contact, help_text)
-        phone = check_phone(content)
-        url = change_url(content)
-        cphone = check_points(content)
-        if "查询" in content:
-            try:
-                pointsres = points_back(cphone)
-                bot.SendTo(contact, pointsres)
-            except:
-                bot.SendTo(contact, check_point_error_text)
+    if not bot.isMe(contact, member):
+        if contact.ctype == "buddy":
+            if "帮助" in content:
+                bot.SendTo(contact, help_text)
+            phone = check_phone(content)
+            url = change_url(content)
+            cphone = check_points(content)
+            if "查询" in content:
+                try:
+                    points_res = points_back(cphone)
+                    bot.SendTo(contact, points_res)
+                except:
+                    bot.SendTo(contact, check_point_error_text)
 
-        if phone and not "查询" in content:
-            code, res = take_bind_id(phone, contact.uin, 1)
-            bot.SendTo(contact, res)
-        if url:
-            url = check_url(url)
-            if url:
-                bot.SendTo(contact, doing_hongbao_text)
-                code, res = bot_get_hongbao(contact.uin, 1, url)
+            if phone and not "查询" in content:
+                code, res = take_bind_id(phone, contact.uin, 1)
                 bot.SendTo(contact, res)
-            else:
-                bot.SendTo(contact, share_error_url_text)
-    elif not bot.isMe(contact, member) and contact.ctype == "group":
-        phone = check_phone(content)
-        url = change_url(content)
-        cphone = check_points(content)
-        if "查询" in content and cphone:
-            try:
-                pointsres = points_back(cphone)
-                bot.SendTo(contact, pointsres)
-            except:
-                bot.SendTo(contact, check_point_error_text)
-
-        if phone and not "查询" in content:
-            code, res = take_bind_id(phone, contact.uin, 1)
-            bot.SendTo(contact, res)
-        if url:
-            url = check_url(url)
             if url:
-                bot.SendTo(contact, member.name + "：" + doing_hongbao_text)
-                code, res = bot_get_hongbao(member.uin, 1, url)
-                bot.SendTo(contact, member.name + "：" + res)
-            else:
-                bot.SendTo(contact, member.name + ":" + share_error_url_text)
+                url = check_url(url)
+                if url:
+                    bot.SendTo(contact, doing_hongbao_text)
+                    code, res = bot_get_hongbao(contact.uin, 1, url)
+                    bot.SendTo(contact, res)
+                else:
+                    bot.SendTo(contact, share_error_url_text)
+        elif contact.ctype == "group":
+            phone = check_phone(content)
+            url = change_url(content)
+            cphone = check_points(content)
+            if "查询" in content and cphone:
+                try:
+                    points_res = points_back(cphone)
+                    bot.SendTo(contact, points_res)
+                except:
+                    bot.SendTo(contact, check_point_error_text)
+
+            if phone and not "查询" in content:
+                code, res = take_bind_id(phone, contact.uin, 1)
+                bot.SendTo(contact, res)
+            if url:
+                url = check_url(url)
+                if url:
+                    bot.SendTo(contact, member.name + "：" + doing_hongbao_text)
+                    code, res = bot_get_hongbao(member.uin, 1, url)
+                    bot.SendTo(contact, member.name + "：" + res)
+                else:
+                    bot.SendTo(contact, member.name + "：" + share_error_url_text)
 
 
-# 生产环境
-# RunBot()
+if __name__ == "__main__":
+    # 生产环境
+    # RunBot()
 
-# 测试环境
-bot.Login(["-q", "2390225401"])
-bot.Run()
+    # 测试环境
+    bot.Login(["-q", "2390225401"])
+    bot.Run()
