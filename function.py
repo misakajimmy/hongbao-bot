526e56190a82468ec2253cc6287f15d05e5e0a06
taoting <335354289@qq.com>
2018-03-29 13:42:48 +0800

    fix bugs

--- function.py
+++ function.py
@@ -1,22 +1,7 @@
-import pymysql
 import requests
 import json
 import re
 from text_config import *
-
-
-def points_back(phone):
-    data = {
-        "phone": phone,
-        "url": ""
-    }
-    r = requests.get("http://hb-api.newitd.com/user_info", data, timeout=30)
-    if r.status_code == 200:
-        json_result = r.text[5:-1]
-        js_res = json.loads(json_result)
-        return js_res["info"]
-    else:
-        return -1, "fail to check points"
 
 
 def check_points(str):
@@ -57,62 +42,63 @@ def change_url(url):
         return False
 
 
-def bind_id(phone, id, type):
-    # 建立数据库连接
-    try:
-        db = pymysql.connect(db_host, db_user, db_password, db_database, charset="utf8")
-        cursor = db.cursor()
-    except:
-        return -1, fail_to_connect_mysql_text
-    if type == 1:
-        sql = "SELECT phone FROM bot WHERE qqid=%s"
+def points_back(phone):
+    data = {
+        "phone": phone,
+    }
+    r = requests.get("http://hb-api.newitd.com/user_info", data, timeout=30)
+    if r.status_code == 200:
+        json_result = r.text[5:-1]
+        js_res = json.loads(json_result)
+        return js_res["code"], js_res["info"]
     else:
-        sql = "SELECT phone FROM bot WHERE wxid=%s"
-    cursor.execute(sql, id)
-    result = cursor.fetchall()
-    if not result:
-        if type == 1:
-            sql = "INSERT INTO bot(qqid, phone) VALUES (%s,%s)"
-        else:
-            sql = "INSERT INTO bot(wxid, phone) VALUES (%s,%s)"
-        cursor.execute(sql, (id, phone))
-        db.commit()
-        return 1, success_insert_bind
+        return -500, fail_to_connect_server_text
+
+
+def take_bind_id(phone, id, type):
+    data = {
+        "phone": phone,
+        "id": id,
+        "type": type
+    }
+    r = requests.get("http://hb-api.newitd.com/take_bind_id", data, timeout=30)
+    if r.status_code == 200:
+        json_result = r.text[5:-1]
+        js_res = json.loads(json_result)
+        return js_res["code"], js_res["info"]
     else:
-        if type == 1:
-            sql = "UPDATE bot SET phone=%s WHERE qqid=%s"
+        return -500, fail_to_connect_server_text
+
+
+def get_bind_id(id, type):
+    data = {
+        "id": id,
+        "type": type
+    }
+    r = requests.get("http://hb-api.newitd.com/get_bind_id", data, timeout=30)
+    if r.status_code == 200:
+        json_result = r.text[5:-1]
+        js_res = json.loads(json_result)
+        if js_res["code"] != 1:
+            return js_res["code"], js_res["info"]
         else:
-            sql = "UPDATE bot SET phone=%s WHERE wxid=%s"
-        cursor.execute(sql, (phone, id))
-        db.commit()
-        return 2, success_update_bind
+            return js_res["code"], js_res["phone"]
+    else:
+        return -500, fail_to_connect_server_text
 
 
 def bot_get_hongbao(id, type, url):
-    # 建立数据库连接
-    try:
-        db = pymysql.connect(db_host, db_user, db_password, db_database, charset="utf8")
-        cursor = db.cursor()
-    except:
-        return -1, "连接数据库失败！请联系管理员！"
-    if type == 1:
-        sql = "SELECT phone FROM bot WHERE qqid=%s"
+    code, phone = get_bind_id(id, type)
+    if code != 1:
+        return code, phone
+    data = {
+        "phone": phone,
+        "url": url,
+    }
+    r = requests.get("http://hb-api.newitd.com/hongbao", data, timeout=30)
+    if r.status_code == 200:
+        json_result = r.text[5:-1]
+        js_res = json.loads(json_result)
+        return js_res["code"], js_res["info"]
     else:
-        sql = "SELECT phone FROM bot WHERE wxid=%s"
-    cursor.execute(sql, id)
-    result = cursor.fetchall()
-    if result:
-        phone = result[0][0]
-        data = {
-            "phone": phone,
-            "url": url,
-        }
-        result = requests.get("http://hb-api.newitd.com/hongbao", data, timeout=30)
-        if result.status_code == 200:
-            json_result = result.text[5:-1]
-            js_res = json.loads(json_result)
-            return 1, js_res['info']
-        else:
-            return -2, "服务器出错，请联系管理员！"
-    else:
-        return -3, "未绑定手机，请输入手机号绑定！"
+        return -500, fail_to_connect_server_text
--- qq_bot.py
+++ qq_bot.py
@@ -13,44 +13,44 @@ def onQQMessage(bot, contact, member, content):
         cphone = check_points(content)
         if "查询" in content:
             try:
-                pointsres = pointsback(cphone)
+                pointsres = points_back(cphone)
                 bot.SendTo(contact, pointsres)
             except:
-                bot.SendTo(contact, check_point_error)
+                bot.SendTo(contact, check_point_error_text)
 
         if phone and not "查询" in content:
-            code, res = bind_id(phone, contact.uin, 1)
+            code, res = take_bind_id(phone, contact.uin, 1)
             bot.SendTo(contact, res)
         if url:
             url = check_url(url)
             if url:
-                bot.SendTo(contact, doing_hongbao)
+                bot.SendTo(contact, doing_hongbao_text)
                 code, res = bot_get_hongbao(contact.uin, 1, url)
                 bot.SendTo(contact, res)
             else:
-                bot.SendTo(contact, share_error_url)
+                bot.SendTo(contact, share_error_url_text)
     elif not bot.isMe(contact, member) and contact.ctype == "group":
         phone = check_phone(content)
         url = change_url(content)
         cphone = check_points(content)
         if "查询" in content and cphone:
             try:
-                pointsres = pointsback(cphone)
+                pointsres = points_back(cphone)
                 bot.SendTo(contact, pointsres)
             except:
-                bot.SendTo(contact, check_point_error)
+                bot.SendTo(contact, check_point_error_text)
 
         if phone and not "查询" in content:
-            code, res = bind_id(phone, contact.uin, 1)
+            code, res = take_bind_id(phone, contact.uin, 1)
             bot.SendTo(contact, res)
         if url:
             url = check_url(url)
             if url:
-                bot.SendTo(contact, member.name + "：" + doing_hongbao)
+                bot.SendTo(contact, member.name + "：" + doing_hongbao_text)
                 code, res = bot_get_hongbao(member.uin, 1, url)
                 bot.SendTo(contact, member.name + "：" + res)
             else:
-                bot.SendTo(contact, member.name + ":" + share_error_url)
+                bot.SendTo(contact, member.name + ":" + share_error_url_text)
 
 
 # 生产环境
